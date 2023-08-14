f69d3b338ccb38de5469d36bf5bcfe6bde02f12e
Diego Garcia <dagadev@proton.me>
2023-08-13 20:35:43 -0500

    Modelos, controladores y rutas de usuario modified

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -74,3 +74,107 @@ export const userDelete = (req, res = response) => {
     })
 
 };
+
+// Crear tipo de usuario
+export const userTypeCreate = (req, res = response) => {
+    // Obtener los datos del body
+    const { name } = req.body;
+    // Crear el tipo de usuario en la base de datos
+    const user = User.createUserType({ name });
+    // Responder con el tipo de usuario creado
+    res.json({
+        msg: 'POST API - controlador: Tipo de usuario creado correctamente',
+        user
+    })
+};
+
+// Obtener todos los tipos de usuario
+export const userTypeGetAll = async (req, res = response) => {
+    // Buscar todos los tipos de usuario en la base de datos
+    const user = await User.readAllUserTypes();
+    // Responder con los tipos de usuario encontrados
+    res.json({
+        msg: 'GET API - controlador: Todos los tipos de usuario',
+        data: user
+    })
+
+};
+
+// Obtener tipo de usuario por Id
+export const userTypeGetById = async (req, res = response) => {
+    // Obtener el ID de la URL
+    const id = parseInt(req.params.id);
+    // Buscar el tipo de usuario en la base de datos
+    const user = await User.getUserTypeById(id);
+    // Responder con el tipo de usuario encontrado
+    res.json({
+        msg: 'GET API - controlador: Tipo de usuario encontrado correctamente',
+        user
+    })
+}
+
+// Eliminar tipo de usuario por Id
+export const userTypeDelete = (req, res = response) => {
+    // Obtener el ID de la URL
+    const id = parseInt(req.params.id);
+    // Eliminar el tipo de usuario en la base de datos
+    const user = User.deleteUserTypeById(id);
+    // Responder con el tipo de usuario eliminado
+    res.json({
+        msg: 'DELETE API - controlador: Tipo de usuario eliminado correctamente',
+        user
+    })
+
+}
+
+// Crear un genero de usuario
+export const userGenderCreate = async (req, res = response) => {
+    // Obtener los datos del body
+    const { name } = req.body;
+    // Crear el genero de usuario en la base de datos
+    const user = await User.createUserGender({ name });
+    // Responder con el genero de usuario creado
+    res.json({
+        msg: 'POST API - controlador: Genero de usuario creado correctamente',
+        user
+    })
+}
+
+// Obtener todos los generos de usuario
+export const userGenderGetAll = async (req, res = response) => {
+    // Buscar todos los generos de usuario en la base de datos
+    const user = await User.readAllUserGenders();
+    // Responder con los generos de usuario encontrados
+    res.json({
+        msg: 'GET API - controlador: Todos los generos de usuario',
+        data: user
+    })
+
+}
+
+// Obtener genero de usuario por Id
+export const userGenderGetById = async (req, res = response) => {
+    // Obtener el ID de la URL
+    const id = parseInt(req.params.id);
+    // Buscar el genero de usuario en la base de datos
+    const user = await User.getUserGenderById(id);
+    // Responder con el genero de usuario encontrado
+    res.json({
+        msg: 'GET API - controlador: Genero de usuario encontrado correctamente',
+        user
+    })
+}
+
+// Eliminar genero de usuario por Id
+export const userGenderDelete = async (req, res = response) => {
+    // Obtener el ID de la URL
+    const id = parseInt(req.params.id);
+    // Eliminar el genero de usuario en la base de datos
+    const user = await User.deleteUserGenderById(id);
+    // Responder con el genero de usuario eliminado
+    res.json({
+        msg: 'DELETE API - controlador: Genero de usuario eliminado correctamente',
+        user
+    })
+
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,11 +9,22 @@ import {
 
 
 const userRouter = Router();
-
+// CRUD de usuarios
 userRouter.get('/find/id/:user_id', userGetById);
 userRouter.get('/find/mail/:email', userGetByEmail);
 userRouter.get('/find/all', userGetAll);
 userRouter.put('/update/:user_id', userUpdate);
 userRouter.delete('/delete/:user_id', userDelete);
 
+// CRUD de tipos de usuario
+userRouter.post('/type/create', createUserType);
+userRouter.get('/type/find/all', readAllUserTypes);
+userRouter.get('/type/find/id/:id', getUserTypeById);
+userRouter.delete('/type/delete/:id', deleteUserTypeById);
+
+// CRUD de generos de usuario
+userRouter.post('/genre/create', createUserGenre);
+userRouter.get('/genre/find/all', readAllUserGenres);
+userRouter.get('/genre/find/id/:id', getUserGenreById);
+userRouter.delete('/genre/delete/:id', deleteUserGenreById);
 export default userRouter;
