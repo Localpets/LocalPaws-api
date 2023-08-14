d411406c45b330b510026bcb82dbd94c421e588c
Diego Garcia <dagadev@proton.me>
2023-08-14 13:18:19 -0500

    I hate this shit

--- src/models/server.model.js
+++ src/models/server.model.js
@@ -1,7 +1,7 @@
 // Paquetes de Node.js
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import cors from 'cors'
 import cookieParser from 'cookie-parser';
 
 // Rutas de la API
