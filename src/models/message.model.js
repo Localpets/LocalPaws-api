c4742253066cd56d10e05572f7aa497e232236f3
Ricardoarsv <ricardoarsv.2004@gmail.com>
2023-09-01 22:14:08 -0500

    Socket.io Implementado y componentes separados

---
+++ src/models/message.model.js
@@ -0,0 +1,111 @@
+// Importar PrismaClient para poder hacer consultas a la base de datos
+import { PrismaClient } from "@prisma/client";
+
+// Importa el cliente de Socket.IO
+import { io } from "socket.io-client";
+
+
+// Crear una nueva instancia de PrismaClient
+const prisma = new PrismaClient();
+
+// Crear la clase Message, que se encargará de manejar los mensajes de los usuarios
+class Message {
+    constructor(sender_id, receiver_id, content) {
+        this.sender_id = sender_id;
+        this.receiver_id = receiver_id;
+        this.text = content;
+    }
+
+// Métodos para crear, leer, actualizar y eliminar comentarios
+    static async createMessage(sender_id, receiver_id, content) {
+        const message = await prisma.message.create({
+            data: {
+                sender_id,
+                receiver_id,
+                text: content
+            }
+        });
+
+        // Emitir el mensaje a los usuarios correspondientes usando Socket.IO
+        const socket = io('http://localhost:7000'); // Cambia esto por la URL de tu servidor
+        socket.emit('newMessage', message); // 'newMessage' es el evento que escucharán tus clientes
+
+        return message;
+    }
+
+
+    static async getAllMessages() {
+        return prisma.message.findMany();
+    }
+
+    static async getAllMessagesByUserId(userId) {
+        return prisma.message.findMany({
+            where: {
+                OR: [
+                    { sender_id: userId },
+                    { receiver_id: userId }
+                ]
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+    }
+
+    static async updateMessage(messageId, newText, edited) {
+        const updatedMessage = await prisma.message.update({
+            where: { id: messageId },
+            data: { text: newText, edited: edited }
+        });
+
+        const socket = io('http://localhost:7000');
+        socket.emit('messageUpdated', updatedMessage); // Emitir evento 'messageUpdated'
+
+        return updatedMessage;
+    }
+
+    static async deleteMessage(messageId) {
+        const deletedMessage = await prisma.message.delete({
+            where: { id: messageId }
+        });
+
+        const socket = io('http://localhost:7000');
+        socket.emit('messageDeleted', deletedMessage.id); // Emitir evento 'messageDeleted'
+
+        return deletedMessage;
+    }
+
+    static async addReaction(messageId, userId, reaction) {
+        const newReaction = await prisma.message_like.create({
+            data: {
+                message_id: messageId,
+                user_id: userId,
+                Reaction: reaction
+            }
+        });
+
+        const socket = io('http://localhost:7000');
+        socket.emit('reactionAdded', newReaction); // Emitir evento 'reactionAdded'
+
+        return newReaction;
+    }
+
+    static async GetMessagesReaction(messageId) {
+        return  prisma.message_like.findMany({
+            where: { message_id: messageId }
+        })
+    }
+    
+    static async removeReaction(messageId) {
+        await prisma.message_like.deleteMany({
+            where: { message_id: messageId}
+        });
+
+        const socket = io('http://localhost:7000');
+        socket.emit('reactionRemoved', { messageId}); // Emitir evento 'reactionRemoved'
+
+        return { messageId};
+    }
+    
+}
+export default Message;
--- src/models/server.model.js
+++ src/models/server.model.js
@@ -1,10 +1,11 @@
-// Paquetes de Node.js
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import http from 'http';
+import { Server as ServerIO } from 'socket.io';
 
-// Rutas de la API
+// Importa las rutas de la API
 import authRouter from '../routes/auth.routes.js';
 import userRouter from '../routes/user.routes.js';
 import commentRouter from '../routes/comment.routes.js';
@@ -12,10 +13,10 @@ import locationRouter from '../routes/location.routes.js';
 import postRouter from '../routes/post.routes.js';
 import likeRouter from '../routes/like.routes.js';
 import followRouter from '../routes/follow.routes.js';
+import chatRouter from '../routes/message.routes.js';
 
 // Acceso a variables de entorno
 dotenv.config();
-
 
 class Server {
     constructor() {
@@ -23,53 +24,102 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
 
-        // Routes
-        this.authRoutePath = '/api/auth';
-        this.userRoutePath = '/api/user';
-        this.commentRoutePath = '/api/comment';
-        this.locationRoutePath = '/api/location';
-        this.postRoutePath = '/api/post';
-        this.likeRoutePath = '/api/like';
-        this.followRoutePath = '/api/follow';
+        // Crear el servidor HTTP
+        this.httpServer = http.createServer(this.app);
+
+        // Configurar la lista blanca de dominios permitidos para CORS
         this.whiteList = ['http://localhost:5173', 'https://localpaws-api-vdfi-dev.fl0.io/', '15.197.201.241:443', 'http://localhost:3000'];
 
-        // Middlewares
+        // Crear una instancia de Socket.IO pasándole el servidor HTTP
+        this.io = new ServerIO(this.httpServer, {
+            cors: {
+                origin: this.whiteList,
+                credentials: true
+            }
+        });
+
+        // Configurar los manejadores de eventos de Socket.IO
+        this.configureSocketEvents();
+
+        // Aplicar middlewares
         this.middlewares();
 
-        // Routes
+        // Definir las rutas
         this.routes();
     }
 
-    middlewares() { 
-        // CORS
-        this.app.use(cors({ origin: this.whiteList, credentials: true }))
-        
-        // Body read and parse
+    // Configurar eventos de socket en el servidor
+    configureSocketEvents = () => {
+        this.io.on('connection', (socket) => {
+            socket.on('newMessage', (message) => {
+                console.log('Nuevo mensaje recibido:', message);
+                // Emitir el mensaje a todos los usuarios conectados, incluido el emisor
+                this.io.emit('newMessage', message);
+            });
+    
+            socket.on('messageUpdated', (updatedMessage) => {
+                console.log('Mensaje actualizado:', updatedMessage);
+                // Emitir el mensaje actualizado a todos los usuarios conectados, incluido el emisor
+                this.io.emit('messageUpdated', updatedMessage);
+            });
+    
+            socket.on('messageDeleted', (messageId) => {
+                console.log('Mensaje eliminado:', messageId);
+                // Emitir el ID del mensaje eliminado a todos los usuarios conectados, incluido el emisor
+                this.io.emit('messageDeleted', messageId);
+            });
+    
+            socket.on('reactionAdded', (newReaction) => {
+                console.log('Nueva reacción:', newReaction);
+                // Emitir la nueva reacción a todos los usuarios conectados, incluido el emisor
+                this.io.emit('reactionAdded', newReaction);
+            });
+    
+            socket.on('reactionRemoved', ({ messageId, userId }) => {
+                console.log('Reacción eliminada:', messageId, userId);
+                // Emitir los IDs de mensaje y usuario a todos los usuarios conectados, incluido el emisor
+                this.io.emit('reactionRemoved', { messageId, userId });
+            });
+        });
+    };
+
+    middlewares() {
+        // Middleware de CORS
+        this.app.use(cors({ origin: this.whiteList, credentials: true }));
+
+        // Parsear JSON en el cuerpo de las peticiones
         this.app.use(express.json());
 
-        // Directorio público
+        // Servir archivos estáticos desde el directorio 'public'
         this.app.use(express.static('public'));
 
-        // Cookies
+        // Middleware para manejar cookies
         this.app.use(cookieParser());
     }
 
     routes() {
-        this.app.use(this.authRoutePath, authRouter);
-        this.app.use(this.userRoutePath, userRouter);
-        this.app.use(this.commentRoutePath, commentRouter);
-        this.app.use(this.locationRoutePath, locationRouter);
-        this.app.use(this.postRoutePath, postRouter);
-        this.app.use(this.likeRoutePath, likeRouter);
-        this.app.use(this.followRoutePath, followRouter);
-    }
+        // Configurar las rutas de la API
+        this.app.use('/api/auth', authRouter);
+        this.app.use('/api/user', userRouter);
+        this.app.use('/api/comment', commentRouter);
+        this.app.use('/api/location', locationRouter);
+        this.app.use('/api/post', postRouter);
+        this.app.use('/api/like', likeRouter);
+        this.app.use('/api/follow', followRouter);
+        this.app.use('/api/message', chatRouter);
 
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Pawsplorer app listening on port ${this.port}`);
+        // Agregar las rutas de Socket.IO
+        this.app.use('/socket.io', (req, res) => {
+            // Esta ruta se utiliza para manejar la comunicación de Socket.IO
         });
     }
 
+    listen() {
+        // Iniciar la escucha del servidor HTTP en el puerto especificado
+        this.httpServer.listen(this.port, () => {
+            console.log(`La aplicación Pawsplorer está escuchando en el puerto ${this.port}`);
+        });
+    }
 }
 
 export default Server;
